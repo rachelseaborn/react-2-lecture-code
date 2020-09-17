8fdf19ed44079352a30297cd2ca6077b6c826302
mattcbodily <mattcbodily@gmail.com>
2020-09-17 12:03:03 -0600

    added comments

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        {/* props are passed in the following syntax: propName={propValue}. This will place a key value pair on the props object of the component receiving the props */}
         <Header 
           username={this.state.username} 
           greeting='Hello World!' 
